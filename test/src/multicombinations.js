74de4eb0c53a9cc4a4449f46e02f763660d587d8
a flying potato <80830782+a-flying-potato@users.noreply.github.com>
2024-02-09 00:50:21 +0100

    :robot: test: Allow to test build with AVA.
    These changes were automatically generated by a transform whose code can be found at:
      - https://github.com/make-github-pseudonymous-again/rejuvenate/blob/208a655cd3a6b4d9d7b09f0c999665e682e5c863/src/transforms/ava:test-build.js
    Please contact the author of the transform if you believe there was an error.

--- test/src/multicombinations.js
+++ test/src/multicombinations.js
@@ -2,7 +2,7 @@ import test from 'ava';
 
 import {list} from '@iterable-iterator/list';
 import {range} from '@iterable-iterator/range';
-import {multicombinations} from '../../src/index.js';
+import {multicombinations} from '#module';
 
 const repr = (x) => (Array.isArray(x) ? JSON.stringify(x) : x);
 
